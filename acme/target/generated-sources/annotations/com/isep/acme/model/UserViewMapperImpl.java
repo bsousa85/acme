244ae0eb96ce95e7f85856d9f5e45407e7dad31f
Vinícius Kuchnir <vina200902@gmail.com>
2023-10-12 22:46:56 -0300

    First Commit

--- acme/target/generated-sources/annotations/com/isep/acme/model/UserViewMapperImpl.java
+++ acme/target/generated-sources/annotations/com/isep/acme/model/UserViewMapperImpl.java
@@ -1,12 +1,12 @@
 package com.isep.acme.model;
 
-import javax.annotation.Generated;
+import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-10-10T22:59:21+0100",
-    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_382 (Azul Systems, Inc.)"
+    date = "2023-10-12T22:10:38-0300",
+    comments = "version: 1.4.2.Final, compiler: Eclipse JDT (IDE) 3.35.0.v20230814-2020, environment: Java 17.0.8.1 (Eclipse Adoptium)"
 )
 @Component
 public class UserViewMapperImpl extends UserViewMapper {
@@ -19,11 +19,11 @@ public class UserViewMapperImpl extends UserViewMapper {
 
         UserView userView = new UserView();
 
+        userView.setFullName( user.getFullName() );
         if ( user.getUserId() != null ) {
             userView.setUserId( String.valueOf( user.getUserId() ) );
         }
         userView.setUsername( user.getUsername() );
-        userView.setFullName( user.getFullName() );
 
         return userView;
     }

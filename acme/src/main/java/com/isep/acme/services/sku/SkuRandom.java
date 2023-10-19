2a4d9c888089f8011a2f3d5d8fd469d18fe0d794
Bruno Sousa <bsousa86@Bsousa86.local>
2023-10-19 21:52:45 +0100

    Implementação do algoritmo random para criar skus

--- acme/src/main/java/com/isep/acme/services/sku/SkuRandom.java
+++ acme/src/main/java/com/isep/acme/services/sku/SkuRandom.java
@@ -3,6 +3,7 @@ package com.isep.acme.services.sku;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
+import java.util.Random;
 
 import com.isep.acme.model.Product;
 
@@ -12,7 +13,24 @@ public class SkuRandom implements ISkuGenerator{
     
     @Override
     public String createSku(Product product) {
-        return "aaaaaa";
+
+        final var sku = new StringBuilder();
+        final var random = new Random();
+        final var specialChars = "$&@?<>~!%#";
+
+        for (int i = 0; i < 3; i++) {
+            sku.append(random.nextInt(10));
+            sku.append((char)(random.nextInt(26) + 'a'));
+        }
+
+        for (int i=0; i < 2; i++) {
+            sku.append((char)(random.nextInt(26) + 'a'));
+            sku.append(random.nextInt(10));
+        }
+
+        sku.append(specialChars.charAt(random.nextInt(specialChars.length())));
+
+        return sku.toString();
     }
 
 }
